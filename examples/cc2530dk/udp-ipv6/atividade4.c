338734481182858b6b0295337c483079822f2752
Felipe <felipeh@alunos.utfpr.edu.br>
2016-09-13 13:44:55 -0300

    só deixei mais simples
    ainda há coisas para melhorar,

--- examples/cc2530dk/udp-ipv6/atividade4.c
+++ examples/cc2530dk/udp-ipv6/atividade4.c
@@ -12,7 +12,7 @@
 #define DEBUG DEBUG_PRINT
 #include "net/uip-debug.h"
 
-#define SEND_INTERVAL		10 * CLOCK_SECOND
+#define INTERVALO		10 * CLOCK_SECOND
 #define MAX_PAYLOAD_LEN		40
 
 #define UIP_IP_BUF   ((struct uip_ip_hdr *)&uip_buf[UIP_LLH_LEN])
@@ -20,55 +20,60 @@
 
 static char buf[MAX_PAYLOAD_LEN];
 
- 
-#define LOCAL_CONN_PORT 8802
+#define PORTA 8802
 
-static struct uip_udp_conn *l_conn;
+#define FAZER_IPV6 (uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0x0212, 0x4b00, 0x07b9, 0x5e8d))
+
+static struct uip_udp_conn *conexao;
  
 #define LED_TOGGLE_REQUEST 	(0x79)
-#define LED_SET_STATE 		(0x7A)
-#define LED_GET_STATE 		(0x7B)
-#define LED_STATE 		(0x7C)
+#define LED_SET_STATE 	    	(0x7A)
+#define LED_GET_STATE 	    	(0x7B)
+#define LED_STATE 		        (0x7C)
  
 PROCESS(udp_client_process, "UDP client process");
  
 AUTOSTART_PROCESSES(&udp_client_process);
  
  
-static void tcpip_handler ( void )
+static void aoReceber()
 {
  
  
 
 }
  
-static void timeout_handler(void)
+static void enviarChar(char a)
+{
+  uip_udp_packet_send(conexao, a, 1);
+}
+
+static void aoDispararTimer()
 {  
   memset(buf, 0, MAX_PAYLOAD_LEN); //zera o buffer global
 
   PRINTF("Cliente para [");
   PRINT6ADDR(&g_conn->ripaddr);
   PRINTF("]:%u,", UIP_HTONS(g_conn->rport));
-
+   
+  sprintf(buf, "Mensagem Aqui");
   uip_udp_packet_send(g_conn, buf, MAX_PAYLOAD_LEN);
-  
+  PRINTF("\n")
+  print_local_addresses();
 }
-/*---------------------------------------------------------------------------*/
-
-static void
-print_local_addresses(void)
+ 
+static void print_local_addresses(void) // 
 {
   int i;
   uint8_t state;
   PRINTF("Nodes's IPv6 addresses:\n");
-  for(i = 0; i < UIP_DS6_ADDR_NB; i++) {
+  for(i = 0; i < UIP_DS6_ADDR_NB; i++) 
+  {
     state = uip_ds6_if.addr_list[i].state;
-    if(uip_ds6_if.addr_list[i].isused && (state == ADDR_TENTATIVE || state == ADDR_PREFERRED)) {
-      PRINTF("  \n");
-      PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
-      if(state == ADDR_TENTATIVE) {
-        uip_ds6_if.addr_list[i].state = ADDR_PREFERRED;
-      }
+    if(uip_ds6_if.addr_list[i].isused && (state == ADDR_TENTATIVE || state == ADDR_PREFERRED))
+    {
+      PRINTF("  \n"); PRINT6ADDR(&uip_ds6_if.addr_list[i].ipaddr);
+      if(state == ADDR_TENTATIVE) { uip_ds6_if.addr_list[i].state = ADDR_PREFERRED;}
     }
   }
 }
@@ -79,47 +84,42 @@ PROCESS_THREAD(udp_client_process, ev, data)
   uip_ipaddr_t ipaddr;
 
   PROCESS_BEGIN();
-  PRINTF("UDP client process started\n");
+  
+  PRINTF("Thread Inicializada\n");
 
-  uip_ip6addr(&ipaddr, 0xfe80, 0, 0, 0, 0x0212, 0x4b00, 0x07b9, 0x5e8d);
-  /* new connection with remote host */
-  l_conn = udp_new(&ipaddr, UIP_HTONS(LOCAL_CONN_PORT), NULL);
-  if(!l_conn) {
-    PRINTF("udp_new l_conn error.\n");
+  FAZER_IPV6;
+  
+  conexao = udp_new(&ipaddr, UIP_HTONS(PORTA), NULL);
+  
+  if(!conexao) 
+  {
+    PRINTF("erro ao iniciar udp"\n");
   }
-  udp_bind(l_conn, UIP_HTONS(LOCAL_CONN_PORT));
-
-  PRINTF("Link-Local connection with ");
-  PRINT6ADDR(&l_conn->ripaddr);
-  PRINTF(" local/remote port %u/%u\n",
-         UIP_HTONS(l_conn->lport), UIP_HTONS(l_conn->rport));
-
-  uip_ip6addr(&ipaddr, 0xaaaa, 0, 0, 0, 0x0212, 0x4b00, 0x07b9, 0x5e8d);
-  g_conn = udp_new(&ipaddr, UIP_HTONS(GLOBAL_CONN_PORT), NULL);
-  if(!g_conn) {
-    PRINTF("udp_new g_conn error.\n");
-  }
-  udp_bind(g_conn, UIP_HTONS(GLOBAL_CONN_PORT));
+  udp_bind(conexao, UIP_HTONS(GLOBAL_CONN_PORT));
 
   print_local_addresses();
 
-  PRINTF("Global connection with ");
-  PRINT6ADDR(&g_conn->ripaddr);
-  PRINTF(" local/remote port %u/%u\n",
-         UIP_HTONS(g_conn->lport), UIP_HTONS(g_conn->rport));
+  PRINTF("Socket com: ");
+  PRINT6ADDR(&conexao->ripaddr);
+  PRINTF("porta: %u/%u\n", UIP_HTONS(conexao->lport), UIP_HTONS(conexao->rport));
 
   etimer_set(&et, SEND_INTERVAL);
 
-  while(1) {
+  while(1) 
+  {
     PROCESS_WAIT_EVENT();
-    if(etimer_expired(&et)) {
-      timeout_handler();
+    
+    if(etimer_expired(&et)) 
+    {
+      aoDispararTimer();
       etimer_restart(&et);
-    } else if(ev == tcpip_event) {
-      tcpip_handler();
+    } 
+    else if(ev == tcpip_event)
+    {
+      aoReceber();
     }
   }
 
   PROCESS_END();
 }
-/*---------------------------------------------------------------------------*/
+
